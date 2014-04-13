2adac61751679dd2dc3974af8046f4c354b97223
Azrin Aris <azrin.aris@gmail.com>
2014-04-13 23:59:55 +0800

    DroneMarker - Extend to other fragment
    Enables DroneMarker to be used by other fragments but supplying Drone
    and GoogleMap variable

--- DroidPlanner/src/org/droidplanner/fragments/markers/DroneMarker.java
+++ DroidPlanner/src/org/droidplanner/fragments/markers/DroneMarker.java
@@ -6,6 +6,7 @@ import org.droidplanner.drone.DroneInterfaces.DroneEventsType;
 import org.droidplanner.drone.DroneInterfaces.OnDroneListener;
 import org.droidplanner.fragments.FlightMapFragment;
 
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
@@ -16,10 +17,19 @@ public class DroneMarker implements OnDroneListener {
 	private Marker droneMarker;
 	private FlightMapFragment flightMapFragment;
 	private Drone drone;
+	private GoogleMap mMap;
 
 	public DroneMarker(FlightMapFragment flightMapFragment) {
 		this.flightMapFragment = flightMapFragment;
 		this.drone = flightMapFragment.drone;
+		this.mMap = flightMapFragment.mMap;
+		addMarkerToMap();
+		drone.events.addDroneListener(this);
+	}
+
+	public DroneMarker(Drone vDrone, GoogleMap vMap) {
+		this.mMap = vMap;
+		this.drone = vDrone;
 		addMarkerToMap();
 		drone.events.addDroneListener(this);
 	}
@@ -31,7 +41,7 @@ public class DroneMarker implements OnDroneListener {
 	}
 
 	private void addMarkerToMap() {
-		droneMarker = flightMapFragment.mMap.addMarker(new MarkerOptions()
+		droneMarker = mMap.addMarker(new MarkerOptions()
 				.anchor((float) 0.5, (float) 0.5).position(new LatLng(0, 0))
 				.icon(BitmapDescriptorFactory.fromResource(R.drawable.quad)).visible(false)
 				.flat(true));
@@ -41,8 +51,8 @@ public class DroneMarker implements OnDroneListener {
 	public void onDroneEvent(DroneEventsType event, Drone drone) {
 		switch (event) {
 		case GPS:
-			updatePosition((float)flightMapFragment.drone.orientation.getYaw(),
-					flightMapFragment.drone.GPS.getPosition());
+			updatePosition((float)drone.orientation.getYaw(),
+					drone.GPS.getPosition());
 			break;
 		default:
 			break;
